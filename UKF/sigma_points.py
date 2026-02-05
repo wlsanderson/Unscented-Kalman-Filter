precision fixes

UKF/sigma_points.py
@@ -5,6 +5,7 @@ Edgar Kraft's paper on quaternion UKF's.
 import numpy as np
 import numpy.typing as npt
 import quaternion
+from UKF.ukf_functions import print_c_array
 
 class SigmaPoints:
     __slots__ = (
@@ -69,8 +70,8 @@ class SigmaPoints:
         # generate the sigma points, the first n sigma points are X + scaled_sqrt, the next n are
         # X - scaled_sqrt.
         for i in range(self._n):
-            sigmas[i+1][self._vec_idx] = np.add(X[self._vec_idx], scaled_cholesky_sqrt[:, i][self._vec_idx])
-            sigmas[self._n+i+1][self._vec_idx] = np.subtract(X[self._vec_idx], scaled_cholesky_sqrt[:, i][self._vec_idx])
+            sigmas[i+1][self._vec_idx] = X[self._vec_idx] + scaled_cholesky_sqrt[:, i][self._vec_idx]
+            sigmas[self._n+i+1][self._vec_idx] = X[self._vec_idx] - scaled_cholesky_sqrt[:, i][self._vec_idx]
 
             # to handle the quaternion part, we convert the rotation vector to a quaternion
             # and apply it to the mean quaternion via quaternion multiplication.

UKF/ukf.py
@@ -104,47 +104,65 @@ class UKF:
     def _unscented_transform_F(self, sigmas: npt.NDArray[np.float32], Wm, Wc, X, noise_cov = None):
         # splitting sigma points up into vector states and quaternion states
         vector_sigmas = sigmas[:, self._vec_idx]
-        quat_sigmas = q.from_float_array(sigmas[:, self._quat_idx])
-        quat_state = q.from_float_array(X[self._quat_idx])
+        quat_sigmas = q.from_float_array(np.float32(sigmas[:, self._quat_idx]))
+        quat_state = q.from_float_array(np.float32(X[self._quat_idx]))
         # small delta quaternions are made by multiplying the quaternion sigmas by the
         # conjugate of the current quaternion state
-        delta_quats = quat_sigmas * quat_state.conjugate()
+        quat_state_conj = q.from_float_array(np.array([X[12], -X[13], -X[14], -X[15]], dtype=np.float32))
+        delta_quats = quat_sigmas * quat_state_conj
+
         # delta quaternion rotations are converted into delta rotation vectors
         delta_rotvecs = np.float32(q.as_rotation_vector(delta_quats))
         # the mean of the rotation vector is calculated by multiplying each sigma by each weight
         # this mean cannot be calculated with quaternions directly due to the inability to sum
         # them.
-        mean_delta_rotvec = np.dot(Wm, delta_rotvecs)
+        #mean_delta_rotvec = np.dot(Wm, delta_rotvecs)
+        mean_delta_rotvec = np.float32(np.zeros(3))
+        for i in range(3):
+            for j in range(self._num_sigmas):
+                mean_delta_rotvec[i] += Wm[j] * delta_rotvecs[j][i]
         # mean delta rotation vector is transformed back into a mean delta quaternion and
         # multiplied into the state to get the quaternion prediction
         mean_delta_quat = q.from_rotation_vector(mean_delta_rotvec)
         mean_quat = np.float32(q.as_float_array((mean_delta_quat * quat_state)))
         # the vector portion of the sigma points are calculated normally
-        vector_mean = np.dot(Wm, vector_sigmas)
+        # np.dot(Wm, vector_sigmas) doesn't actually give the expected float32 answer that matches
+        # the C code
+        vector_mean = np.float32(np.zeros(self._dim_x - 4))
+        for i in range(self._dim_x - 4):
+            for j in range(self._num_sigmas):
+                vector_mean[i] += (Wm[j] * vector_sigmas[j][i])
+
         # if there is a control input, then its either standby state or landed state.
         # ideally, this should be handled by checking the flight state.
         x_mean = np.concatenate([vector_mean, mean_quat])
-        
         vec_residual = vector_sigmas - vector_mean[np.newaxis, :]
         full_residuals = np.hstack((vec_residual, delta_rotvecs))
-        P_covariance = np.dot(full_residuals.T, np.dot(np.diag(Wc), full_residuals))
+        # this line is not equivalent to C code, numerical differences
+        # P_covariance = np.dot(full_residuals.T, np.dot(np.diag(Wc), full_residuals))
+        P_covariance = np.zeros((self._dim_x - 1, self._dim_x - 1), dtype=np.float32)
+        for i in range(self._dim_x - 1):
+            for k in range(self._dim_x - 1):
+                val = np.float32(0.0)
+                for j in range(self._num_sigmas):
+                    val += np.float32(Wc[j]) * np.float32(full_residuals[j][i]) * np.float32(full_residuals[j][k])
+                P_covariance[i][k] = val
         if noise_cov is not None:
             P_covariance += noise_cov
         return (x_mean, P_covariance)
     
     @staticmethod
     def _unscented_transform_H(sigmas: npt.NDArray[np.float32], Wm, Wc, noise_cov):
-        x_mean = np.dot(Wm, sigmas)
-        residual = sigmas - x_mean[np.newaxis, :]
+        z_mean = np.dot(Wm, sigmas)
+        residual = sigmas - z_mean[np.newaxis, :]
         P_covariance = np.dot(residual.T, np.dot(np.diag(Wc), residual))
         P_covariance += noise_cov
-        return (x_mean, P_covariance)
+        return (z_mean, P_covariance)
 
     def compute_process_sigmas(self, dt, Q, u):
         sigmas = self._sigma_points_class.calculate_sigma_points(self.X, self.P, Q)
         for i, s in enumerate(sigmas):
             self._sigmas_f[i] = self.F(s, dt, u)
-        print_c_array(self._sigmas_f)
 
     def _calculate_cross_cov(self, x, z):
         """

scripts/run_context.py
@@ -3,45 +3,12 @@ from UKF.plotter import Plotter
 from UKF.data_processor import DataProcessor
 from pathlib import Path
 import numpy as np
+import quaternion as q
+from UKF.ukf_functions import print_c_array
 
-def chol(orig):
-    ret = np.zeros(orig.shape)
-    for i in range(4):
-        ret[i, i] = orig[i, i]
-        for k in range(i):
-            ret[i, i] -= ret[i, k]*ret[i, k]
-        if ret[i][i] <= 0:
-            print("non pos def")
-            return
-        ret[i, i] = np.sqrt(ret[i, i])
-        for j in range(i + 1, 4):
-            ret[j, i] = orig[j, i]
-            for k in range(i):
-                ret[j, i] -= ret[j, k] * ret[i, k]
-            ret[j, i] /= ret[i, i]
-    return ret
-
-
-#a = np.array([[2,1,0,0],[1,3,0,0],[0,0,1,0], [0,0,0,4]])
-s = np.array([[5,2,0,0],[2,5,1,0],[0,1,3,1], [0,0,1,4]])
-pxy = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]])
-# print(pxy @ np.linalg.inv(s))
-print(s)
-print()
-l = np.linalg.cholesky(s)
-y = np.linalg.solve(l, pxy.T)
-#print(np.linalg.solve(l.T, y))
-print(l)
-print()
-print(chol(s))
-
-
-# print("\n")
-
-# residual = np.array([10,20,30,40])
-# print(residual * (np.linalg.inv(s) @ residual))
-# y = np.linalg.solve(l, residual.T)
-# x = np.linalg.solve(l.T, y).T
-# print(residual * x)
-
+quat_a = q.from_float_array(np.array([0.1, 0.2, 0.3, 0.4]))
+quat_b = q.from_float_array(np.array([4.2, -5.6, 0.0, 0.1]))
+quat_a = quat_a.normalized()
+quat_b = quat_b.normalized()
+print(quat_a * quat_b)
 

scripts/test_predict.py
@@ -8,7 +8,7 @@ def process_covariance_function(dt):
         """
         Process noise covariance matrix
         """
-        qvar = np.ones((15, 15)) * 1
+        qvar = np.float32(np.ones((15, 15))) * np.float32(1.0)
         return qvar * dt
 
 sigma_points = SigmaPoints(
@@ -23,29 +23,20 @@ ukf = UKF(
             dim_z = MEASUREMENT_DIM,
             points = sigma_points,
         )
-ukf.P = INITIAL_STATE_COV.copy()
-ukf.X = np.array([0, 0, 4.0, 0, 1.0, 5.5, 1.0, 2.5, -2.5, 6.4, 0.2, -0.3, 0.4, -0.8, -0.1, 0.5])
+ukf.P = np.float32(INITIAL_STATE_COV.copy())
+ukf.X = np.array([0.0, 0.0, 4.0, 0, 1.0, 5.5, 1.0, 2.5, -2.5, 6.4, 0.2, -0.3, 0.4, -0.8, -0.1, 0.5], dtype=np.float32)
 ukf.F = state_transition_function
 ukf.Q = process_covariance_function
-ukf.predict(0.1, 3)
-raise Exception
+ukf.predict(np.float32(0.05), 3)
 print_c_array(ukf.X)
 print()
-
-ukf.predict(0.1, 3)
-# print_c_array(ukf.X)
-# print()
-ukf.predict(0.1, 3)
-# print_c_array(ukf.X)
-# print()
-ukf.predict(0.1, 3)
-
-# print_c_array(ukf.X)
-# print()
-ukf.predict(0.1, 3)
-# print_c_array(ukf.X)
-# print()
-# print_c_array(ukf.P)
+ukf.predict(0.05, 3)
+print_c_array(ukf.X)
+print()
+ukf.predict(0.05, 3)
+print_c_array(ukf.X)
+print()
+print_c_array(ukf.P)
 
 
 